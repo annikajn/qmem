77d7eb56548db6de0e7f34b931b35cbb0a84b71e
annikanic <14ajn@queensu.ca>
2019-02-13 13:30:40 -0500

    Fix bugs on local, testing on heroku

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,7 +57,7 @@ class Kernel extends HttpKernel
         // 'can' => \Illuminate\Auth\Middleware\Authorize::class,
         // 'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         // 'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'auth' => \App\Http\Middleware\id::class,
+        'auth' => \App\Http\Middleware\AuthNetid::class,
         'auth.admin' => \App\Http\Middleware\AuthAdmin::class,
         'auth.user' => \App\Http\Middleware\AuthUser::class,
     ];
